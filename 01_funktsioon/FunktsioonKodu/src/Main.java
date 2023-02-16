b935839ebd946c9a3de4b7bf1b85a8d1260d52ed
Sander Sims <114922010+sandersims@users.noreply.github.com>
2023-02-16 17:39:30 +0200

    Esimene kodutöö tehtud

--- 01_funktsioon/FunktsioonKodu/src/Main.java
+++ 01_funktsioon/FunktsioonKodu/src/Main.java
@@ -2,9 +2,33 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
 
-        for (int i = 0; i < 5; i++) {
-            System.out.println(i);
+        int vahemaa = 100;
+        int aeg = 2;
+        System.out.println("Kui läbid " + vahemaa + "km " + aeg + " tunniga siis su kiirus on: " + kiiruseLeidmine(vahemaa, aeg) + "km/h");
 
+        int raskus = 120;
+        System.out.println( raskuseHinnang(raskus));
+
+        int arv1 = 5;
+        int arv2 = 7;
+        System.out.println(arv1 + " * " + arv2 + " = " + korrutamine(arv1, arv2));
+    }
+
+    private static int korrutamine(int arv1, int arv2) {
+        return arv1 * arv2;
+    }
+
+    private static String raskuseHinnang(int raskus) {
+        String hinnang;
+        if (raskus > 100) {
+            hinnang = "Raske raskus";
+        } else {
+            hinnang = "Kerge raskus";
         }
+        return hinnang;
+    }
+
+    private static double kiiruseLeidmine ( int vahemaa, int aeg){
+        return (double) vahemaa / (double) aeg;
     }
 }
